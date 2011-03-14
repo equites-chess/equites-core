2d4c50e8bc49cb4781e6f06741f0070d2c279bc0
Frank S. Thomas <fst@debian.org>
2011-03-14 12:09:38 +0100

    Override more paths.

--- project/build/Equites.scala
+++ project/build/Equites.scala
@@ -2,4 +2,8 @@ import sbt._
 
 class EquitesProject(info: ProjectInfo) extends DefaultProject(info) {
   override def mainScalaSourcePath = "src"
+  override def mainResourcesPath   = "resources"
+
+  override def testScalaSourcePath = "test"
+  override def testResourcesPath   = "test-resources"
 }
