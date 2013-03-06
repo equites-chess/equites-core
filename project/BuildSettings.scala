8adb1a85bcdea85ec640e925b4e288ed57361077
Frank S. Thomas <frank@timepit.eu>
2013-03-06 21:27:21 +0100

    Added homepage.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,6 +10,7 @@ object BuildSettings {
       scalaVersion := "2.10.0",
       scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
 
+      homepage := Some(url("http://equites.timepit.eu")),
       startYear := Some(2011),
       licenses += "GPLv3" -> url("http://www.gnu.org/licenses/gpl-3.0.html")
     )
