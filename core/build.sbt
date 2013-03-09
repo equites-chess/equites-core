4b53dcc99aa0a2693f4129a3a1f4902a8aba49f4
Frank S. Thomas <frank@timepit.eu>
2013-03-09 07:40:49 +0100

    Added ScalaCheck.

--- core/build.sbt
+++ core/build.sbt
@@ -1,12 +1,9 @@
 name := "Equites-core"
 
-version := "0.0"
-
 scalaVersion := "2.10.0"
 
 libraryDependencies ++= Seq(
-    "org.scalaz" % "scalaz-core_2.10" % "7+",
-    "org.specs2" % "specs2_2.10" % "1.13"
+    "org.scalaz" % "scalaz-core_2.10" % "7+"
 )
 
 scalacOptions in (Compile, doc) ++= Seq("-diagrams")
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -4,18 +4,22 @@ import Keys._
 import org.scalastyle.sbt.ScalastylePlugin
 
 object BuildSettings {
-  lazy val basicSettings =
-    ScalastylePlugin.Settings ++
-    seq(
-      scalaVersion := "2.10.0",
-      scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
+  lazy val basicSettings = ScalastylePlugin.Settings ++ seq(
+    scalaVersion := "2.10.0",
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
 
-      homepage := Some(url("http://equites.timepit.eu")),
-      startYear := Some(2011),
-      licenses += "GPLv3" -> url("http://www.gnu.org/licenses/gpl-3.0.html")
-    )
+    version := "0.0",
+    homepage := Some(url("http://equites.timepit.eu")),
+    startYear := Some(2011),
+    licenses += "GPLv3" -> url("http://www.gnu.org/licenses/gpl-3.0.html")
+  )
 
   lazy val coreSettings = basicSettings ++ seq(
+    libraryDependencies ++= Seq(
+      "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
+      "org.specs2" %% "specs2" % "1.14" % "test"
+    ),
+
     initialCommands := """
       import scalaz._
       import Scalaz._
