e5f74e222b9bb8a28a5ae3df8ccebfe17bc34614
Frank S. Thomas <frank@timepit.eu>
2013-06-22 23:18:36 +0200

    Test CoordinateMove.

---
+++ core/src/test/scala/eu/timepit/equites/util/CoordinateMoveSpec.scala
@@ -0,0 +1,41 @@
+// Equites, a Scala chess playground
+// Copyright © 2013 Frank S. Thomas <frank@timepit.eu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package eu.timepit.equites
+package util
+
+import org.specs2.mutable._
+
+class CoordinateMoveSpec extends Specification {
+  "CoordinateMove companion" should {
+    "correctly apply promotions" in {
+      val promotion =
+        Promotion(Pawn(White), Square(0, 6), Square(0, 7), Rook(White))
+      CoordinateMove(promotion) must_==
+        CoordinateMove(Square(0, 6), Square(0, 7), Some(Rook(White)))
+    }
+
+    "correctly apply moves" in {
+      val move = Move(Queen(White), Square(0, 0), Square(3, 3))
+      CoordinateMove(move) must_== CoordinateMove(Square(0, 0), Square(3, 3))
+    }
+
+    "correctly apply castlings" in {
+      val castling = CastlingLong(White)
+      CoordinateMove(castling) must_== CoordinateMove(castling.kingMove)
+    }
+  }
+}
