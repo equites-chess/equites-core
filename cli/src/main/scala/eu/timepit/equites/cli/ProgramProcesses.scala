37c0928a74099232c9d7bbdd0e300ba410e507ee
Frank S. Thomas <frank@timepit.eu>
2013-12-18 06:44:14 +0100

    Work on Subprocess

--- cli/src/main/scala/eu/timepit/equites/cli/ProgramProcesses.scala
+++ cli/src/main/scala/eu/timepit/equites/cli/ProgramProcesses.scala
@@ -1,7 +1,43 @@
-package eu.timepit.equites.cli
+package eu.timepit.equites
+package cli
 
 import scalaz.concurrent.Task
 import scalaz.stream._
+
+object Subprocess {
+  def popen(args: String*): Process[Task, Subprocess] = {
+    Task.delay(new Subprocess(args: _*))
+    ???
+  }
+}
+
+class Subprocess(args: String*) {
+  val input: Sink[Task, Array[Byte]] = inputFrom(procIn)
+
+  def close: Int = {
+    procIn.close
+    procOut.close
+    procErr.close
+    proc.waitFor
+  }
+
+  private[this] def inputFrom(out: java.io.OutputStream) =
+    io.channel {
+      (bytes: Array[Byte]) => Task.delay {
+        out.write(bytes)
+        out.flush
+      }
+    }
+
+  private[this] val proc = new java.lang.ProcessBuilder(args: _*).start
+
+  private[this] val procIn = proc.getOutputStream
+  private[this] val procOut = proc.getInputStream
+  private[this] val procErr = proc.getErrorStream
+}
+
+
+
 
 object ProgramProcesses {
 
@@ -29,7 +65,7 @@ object ProgramProcesses {
           (streamReader, bufferedReader)
         }
       } {
-        case (_ , br) => Task.delay { () }
+        case (_ , br) => Task.delay { println("release InputStream") }
       } {
         case (sr , br) => {
           Task.delay {
--- cli/src/main/scala/eu/timepit/equites/cli/UciEngineVsItself.scala
+++ cli/src/main/scala/eu/timepit/equites/cli/UciEngineVsItself.scala
@@ -28,29 +28,42 @@ object UciEngineVsItself extends App {
 
   type SimpleHistory = Vector[GameState]
 
+  /* TODO
+   * - better names for read write
+   * - inputPipe
+   * - outputPipe
+   * - errorPipe
+   * - proc.input
+   * - proc.output
+   * - proc.error
+   * Subprocess
+   * popen
+  */
+  
   val (proc, write, read) = ProgramProcesses.system("gnuchess", "-u")
   //val (proc, write, read) = ProgramProcesses.system("fruit")
 
   def writeGo = toRawCommands(Go(Go.Movetime(350))).through(write)
   def readResponses = read.pipe(collectResponses)
   def readUntilReady = readResponses.find(_ == ReadyOk)
-  def readUntilBestmove = readResponses |> collectFirst { case x:Bestmove => x }
+  def readUntilBestmove = readResponses |> collectFirst { case bm: Bestmove => bm }
 
   def playGame(hist: Seq[GameState]): Process[Task, Seq[GameState]] = {
     toRawCommands(Position(hist)).through(write)
     .append(writeGo)
-    .append(Process.emit(hist).zip(readUntilBestmove).pipe(appendMove))
+    .append(Process(hist).zip(readUntilBestmove).pipe(appendMove))
     .collect { case x: SimpleHistory => x }
     .observe(stdOutLastBoard)
     .flatMap(playGame)
   }
 
-  newGameCommands.through(write)
+  (newGameCommands.through(write)
     .append(readUntilReady)
-    .append(playGame(Vector(GameState.init)))
-    .append(toRawCommands(Quit).through(write))
+    .append(playGame(Vector(GameState.init))))
+    .onComplete(toRawCommands(Quit).through(write))
     .run.run
 
   proc.destroy
 
 }
+ 
