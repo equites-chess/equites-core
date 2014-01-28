124f1103f96da15eb7a645ed31c811a2f53014d2
Frank S. Thomas <frank@timepit.eu>
2014-01-28 16:49:01 +0100

    Also build with the oraclejdk7

--- core/src/test/scala/eu/timepit/equites/util/PackageSpec.scala
+++ core/src/test/scala/eu/timepit/equites/util/PackageSpec.scala
@@ -1,5 +1,5 @@
 // Equites, a Scala chess playground
-// Copyright © 2013 Frank S. Thomas <frank@timepit.eu>
+// Copyright © 2013-2014 Frank S. Thomas <frank@timepit.eu>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
