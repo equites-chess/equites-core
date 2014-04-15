fb16accf1ba4433805dba12ddb4268a2b5cb53fd
Frank S. Thomas <frank@timepit.eu>
2014-04-15 03:27:36 +0200

    Update to Scala 2.10.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val scalazStream =
     "org.scalaz.stream" %% "scalaz-stream" % "0.3.1"
   val shapeless =
-    "com.chuusai" % "shapeless" % "2.0.0-M1" cross CrossVersion.full
+    "com.chuusai" % "shapeless" % "2.0.0" cross CrossVersion.full
   val specs2 =
     "org.specs2" %% "specs2" % "2.2.2"
   val swaggerPlay =
