9269342001822f56ab7b583207411549da5a8057
Frank S. Thomas <frank@timepit.eu>
2015-05-07 22:13:33 +0200

    Whoops

---
+++ shared/src/main/scala/eu/timepit/equites/util/TextCommand.scala
@@ -0,0 +1,29 @@
+// Equites, a Scala chess playground
+// Copyright © 2013, 2015 Frank S. Thomas <frank@timepit.eu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package eu.timepit.equites
+package util
+
+trait TextCommand extends Product {
+  def cmdName: String =
+    productPrefix.toLowerCase
+
+  def cmdArgs: Seq[String] =
+    productIterator.toSeq.map(_.toString)
+
+  override def toString: String =
+    cmdName + cmdArgs.map(" " + _).mkString
+}
